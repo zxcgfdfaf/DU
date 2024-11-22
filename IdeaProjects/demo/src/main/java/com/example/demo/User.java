d4833eb36418d4529a8be8e0b74a812bc1897915
zxcgfdfaf <akimixqq@gmail.com>
2024-11-22 17:10:17 +0100

    33

--- IdeaProjects/demo/src/main/java/com/example/demo/User.java
+++ IdeaProjects/demo/src/main/java/com/example/demo/User.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 
 @Entity
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Автоматическая генерация ID
     private Long id;
     private String username;
     private String password;
--- IdeaProjects/demo/src/main/java/com/example/demo/UserRepository.java
+++ IdeaProjects/demo/src/main/java/com/example/demo/UserRepository.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, String> {
-    User findByUsername(String username);
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);  // Найти пользователя по имени
 }
